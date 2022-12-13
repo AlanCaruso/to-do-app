d8da53a4d525295007d2fa03d8e8e8526ea60f80
Alan Caruso <alan.carusoo@gmail.com>
2022-12-12 23:02:03 -0300

    done state added to the items
    you can now check items

--- main.js
+++ main.js
@@ -1,27 +1,48 @@
 const list = document.querySelector("#task-board")
 const button = document.querySelector(".add-item");
 const input = document.querySelector(".add-item-input");
-const noData = document.querySelector(".no-data")
+const noData = document.querySelector(".no-data");
 
-function addTask(task, done, id) {
+const check = 'fa-check-square';
+const checkColor = 'check-color'
+const uncheck = 'fa-square';
+const lineTrough = 'line-trough';
+let id = 0;
+
+function addTask(task, id, done, deleted) {
+    if (deleted) { return }
+    const DONE = done ? check : uncheck;
+    const LINE = done ? lineTrough : '';
+
     const element = `
         <li class="task">
-            <input type="checkbox" />
-            <p>${task}</p>
-            <span class="delete-button disabled"><i class="far fa-trash-alt"></i></span>
+            <i class="checkbox fa-regular ${DONE}" data="done" id="${id}"></i>
+            <p class="text ${LINE}">${task}</p>
+            <i class="far fa-trash-alt delete-button" data="deleted" id="${id}"></i>
         </li>
     `
+
     list.insertAdjacentHTML("afterbegin", element);
+}
+
+function taskDone(element) {
+    element.classList.toggle(check)
+    element.classList.toggle(uncheck)
+    element.classList.toggle(checkColor)
+    element.parentNode.querySelector('.text').classList.toggle(lineTrough);
+
 }
 
 button.addEventListener("click", () => {
     noData.classList.add("d-none");
+
     const task = input.value
     console.log(task)
     if (task) {
-        addTask(task)
+        addTask(task, id, false, false)
     }
     input.value = ''
+    id++
 })
 
 document.addEventListener('keyup', function (event) {
@@ -30,8 +51,19 @@ document.addEventListener('keyup', function (event) {
         const task = input.value
 
         if (task) {
-            addTask(task)
+            addTask(task, id, false, false)
         }
         input.value = ''
+        id++
+    }
+})
+
+list.addEventListener('click', function (event) {
+    const element = event.target;
+    const elementData = element.attributes.data.value
+    if (elementData === "done") {
+        taskDone(element);
+    } else if (elementData === "deleted") {
+        taskDeleted()
     }
 })
